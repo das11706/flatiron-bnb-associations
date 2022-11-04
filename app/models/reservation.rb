efaffccbd96d0dfe89aa0c7e9699917a8dd69d3e
Daniel Suarez <suarez.daniel101@gmail.com>
2022-11-04 13:55:17 -0400

    commit tree 290c419048f5bc3d01e791a241cdece46c52d995

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,8 @@
 class Reservation < ActiveRecord::Base 
   belongs_to :listing
-  belongs_to :guest, :class_name => :User
+  # belongs_to :guest, :class_name => :User
+  belongs_to :guest, :class_name => "User"
+  has_many :reviews
 end
 
 
